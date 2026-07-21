1bc1a9fb7c6b85f12af3ef9b0d815f97c89e7dd8
Yousra Abdelrahman <yousraagoub@gmail.com>
2026-07-21 07:51:39 -0500

    Remove back to top button functionality
    Removed back to top button functionality from script.js.

--- script.js
+++ script.js
@@ -161,41 +161,6 @@ function updateProgressBar() {
 window.addEventListener("scroll", updateProgressBar);
 
 /* ==========================
-   BACK TO TOP BUTTON
-========================== */
-
-const backToTop =
-    document.querySelector(".back-to-top");
-
-window.addEventListener("scroll", () => {
-
-    if (!backToTop) return;
-
-    if (window.scrollY > 500) {
-
-        backToTop.classList.add("show");
-
-    } else {
-
-        backToTop.classList.remove("show");
-
-    }
-
-});
-
-backToTop?.addEventListener("click", () => {
-
-    window.scrollTo({
-
-        top: 0,
-
-        behavior: "smooth"
-
-    });
-
-});
-
-/* ==========================
    ACTIVE NAV LINK
 ========================== */
 
